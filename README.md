ed2348fc177dbdc0f9e9ca8ef34d6fedc4d782fc
金建业 <jianye.jin@hand-china.com>
2019-12-04 16:13:21 +0800

    日志清除控制

--- README.md
+++ README.md
@@ -1,31 +1,60 @@
-## 前端日志控制
-### 安装
+# 前端日志控制
+## 安装
 ```
 npm install console-log-overwrite --save
 ```
 
 
-### 使用
-网站初始化前执行如下代码
+## 使用
+
+
+
+### 当为生产环境时，不打印`console.log`日志
+
+ES5用法
 ```
 const consoleLogOverwrite = require('console-log-overwrite')
 
-// 当前环境变量为prod，console.log将失效
-// dev环境不影响
+consoleLogOverwrite(process.env.ENV, 'prod', '123')
+```
+> 注意：为确保全局覆盖，重写的调用请放在网站初始化期间
+
+ES6用法
+```
+import { consoleLogOverwrite } from 'console-log-overwrite'
+
 consoleLogOverwrite(process.env.ENV, 'prod', '123')
 
 ```
 
 
-当生产需要查看日志
+
+### 当生产环境需要查看日志
+
+`打开所在页面控制台console`
+
+`输入以下代码启用日志`
 
 ```
-// 去生产页面，打开控制台console，输入以下代码启用日志
-// sessionStorage.setItem('__debugger', '123')
-// 刷新页面，日志启用
-
-// 再次关闭日志
-// sessionStorage.removeItem('__debugger')
+sessionStorage.setItem('__debugger', '123')
 ```
 
+`刷新当前页面日志生效`
 
+`输入以下代码再次关闭日志`
+
+```
+sessionStorage.removeItem('__debugger')
+```
+
+## API
+
+`consoleLogOverwrite(env, envCompare, keygen)`
+
+
+参数|是否必须|默认值|说明
+---|:--:|---:|---
+env|Y|无|环境变量
+envCompare|Y|无|环境变量
+env|Y|无|需要屏蔽的环境变量值，如：'prod'
+keygen|内容|无|生产环境需要查看时的秘钥
--- index.js
+++ index.js
@@ -1,37 +1,16 @@
 /**
  * 控制台日志清除
+ *      - 重写console.log后，仍然可以追踪文件位置
+ *      - 实现本地开发日志打印，上线后不用逐个删除
+ * 
  * author: jyjin
  * date:   2019-12-04
- */
-
-// 方法一
-// 验证可行
-// 但是无法在控制台追踪源代码位置，所有位置都会定位到本文件的 oriLogFunc.call(console, ...arguments); 这一行
-// console.log = (function (oriLogFunc) {
-//   return function () {
-//     console.info('hh')
-//     //判断配置文件是否开启日志调试
-//     if (sessionStorage.getItem('__debugger') === '__h5customer') {
-//       try {
-//         oriLogFunc.call(console, ...arguments);
-//       } catch (e) {
-//         console.error('console.log error', e);
-//       }
-//     }
-//   }
-// })(console.log);
-
-
-/**
- * 对特定环境的日志屏蔽
- * 方法二
  * 
- * 可以定位文件位置
  * @env: 环境变量 process.env.ENV
  * @envCompare: 环境变量 process.env.ENV 匹配的值
  * @keygen: 预留的开关值
  */
-export default consoleLogOverwrite = function (envName, envValue, keygen) {
+exports.consoleLogOverwrite = function (envName, envValue, keygen) {
 
   if (envName === envValue) {
     sessionStorage.removeItem('__debugger')
@@ -64,6 +43,30 @@ export default consoleLogOverwrite = function (envName, envValue, keygen) {
   };
 }
 
-//正式环境屏蔽日志
-// consoleLogOverwrite(process.env.envName, 'production', '__h5im')
+// 调用 正式环境屏蔽日志
+// consoleLogOverwrite(process.env.envName, 'production', '123')
+
+
+
+
+
+
+// 方法一
+//      验证可行
+//      但是无法在控制台追踪源代码位置
+//      所有位置都会定位到本文件的 oriLogFunc.call(console, ...arguments); 这一行
+// 
+// console.log = (function (oriLogFunc) {
+//   return function () {
+//     console.info('hh')
+//     //判断配置文件是否开启日志调试
+//     if (sessionStorage.getItem('__debugger') === '__h5customer') {
+//       try {
+//         oriLogFunc.call(console, ...arguments);
+//       } catch (e) {
+//         console.error('console.log error', e);
+//       }
+//     }
+//   }
+// })(console.log);
 
