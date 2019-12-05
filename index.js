cda36e613b1cf09b28492bc7020246e908e9e52d
金建业 <jianye.jin@hand-china.com>
2019-12-05 16:13:11 +0800

    日志打印保留对象,不转化成字符串

--- index.js
+++ index.js
@@ -34,39 +34,10 @@ exports.consoleLogOverwrite = function (envName, envValue, keygen) {
     var line = matchResult[1] || ""
     for (var i in arguments) {
     }
-    if (typeof arguments[i] == 'object') {
-      arguments[i] = JSON.stringify(arguments[i])
-    }
-    arguments[i] += '\t\t\t\t\t\t\t\t\t\t\t' + line.replace("(", "").replace(")", "")
-    log.apply(console, arguments)
+    log.apply(console, [...arguments, '\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t' + line.replace("(", "").replace(")", "")])
 
   };
 }
 
 // 调用 正式环境屏蔽日志
 // consoleLogOverwrite(process.env.envName, 'production', '123')
-
-
-
-
-
-
-// 方法一
-//      验证可行
-//      但是无法在控制台追踪源代码位置
-//      所有位置都会定位到本文件的 oriLogFunc.call(console, ...arguments); 这一行
-// 
-// console.log = (function (oriLogFunc) {
-//   return function () {
-//     console.info('hh')
-//     //判断配置文件是否开启日志调试
-//     if (sessionStorage.getItem('__debugger') === '__h5customer') {
-//       try {
-//         oriLogFunc.call(console, ...arguments);
-//       } catch (e) {
-//         console.error('console.log error', e);
-//       }
-//     }
-//   }
-// })(console.log);
-
